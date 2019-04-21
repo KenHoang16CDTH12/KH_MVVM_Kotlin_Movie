67a832b50af8449af3182ea1f623736709d94211
Duong Minh Hoang <=>
2019-04-21 13:35:41 +0700

    Write Unit Test API

---
+++ app/src/test/java/com/sun/kh_mvvm_kotlin_movie/data/source/remote/api/ApiUtil.kt
@@ -0,0 +1,13 @@
+package com.sun.kh_mvvm_kotlin_movie.data.source.remote.api
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Response
+
+object ApiUtil {
+  fun <T : Any> successCall(data: T) = createCall(Response.success(data))
+
+  private fun <T : Any> createCall(response: Response<T>) = MutableLiveData<ApiResponse<T>>().apply {
+    value = ApiResponse(response)
+  } as LiveData<ApiResponse<T>>
+}
